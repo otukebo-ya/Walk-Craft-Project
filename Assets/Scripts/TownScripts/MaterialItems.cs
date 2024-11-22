e9c25a8b2d5256daf5b3ea55cd2873a3a12ef94d
skeltonsan <yusukesan92860@gmail.com>
2024-11-22 22:48:03 +0900

    BluePrintにおけるNeedMaterialの追加
    BluePrintに必要素材を管理するNeedMaterialsを追加しました

---
+++ Assets/Scripts/TownScripts/MaterialItems.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class MterialItems
+{
+    public string materialName;
+    public int quantity;
+}
